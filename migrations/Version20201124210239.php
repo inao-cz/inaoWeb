c6ef387beafde93d93a7030774d1de85fef6e32a
Jakub <onemoreplayscz@gmail.com>
2021-02-06 12:22:58 +0100

    Added forgotten files.

---
+++ migrations/Version20201124210239.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201124210239 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE invite ADD username VARCHAR(180) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C7E210D7F85E0677 ON invite (username)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_C7E210D7F85E0677 ON invite');
+        $this->addSql('ALTER TABLE invite DROP username');
+    }
+}
